1483fc005985b119f65706b6b9d37abcb77c45cb
alweiis <jstm1207@gmail.com>
2023-02-03 13:52:53 +0900

    refactor: MyWebconfig.java 수정
    업로드 경로를 리눅스 경로로 변경

--- second/src/main/java/com/example/demo/MyWebConfig.java
+++ second/src/main/java/com/example/demo/MyWebConfig.java
@@ -7,9 +7,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MyWebConfig implements WebMvcConfigurer{
 
+	//for window
+	// public static final String savePath = "c:/upload/"; 
+	
+	//for linux
+	public static final String savePath = "/usr/upload/"; 
+	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/upload/**").addResourceLocations("file:///c:/upload/");
+		// for window
+		// registry.addResourceHandler("/upload/**").addResourceLocations("file:///c:/upload/");
+		
+		// for linux
+		registry.addResourceHandler("/upload/**").addResourceLocations("file:/usr/upload/");
 	}
 
 }
